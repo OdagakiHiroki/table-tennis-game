3df721d5083c09da8b7d91be58d6c488ecbd6bfb
OdagakiHiroki <email@example.com>
2021-01-02 04:45:39 +0900

    add game main base

--- src/assets/js/three.js
+++ src/assets/js/three.js
@@ -1,7 +1,68 @@
 import * as THREE from 'three';
 
 const customThree = {
-
+  // scene
+  createScene() {
+    return new THREE.Scene();
+  },
+  // renderer
+  createWebGLRenderer(params) {
+    return new THREE.WebGLRenderer(params);
+  },
+  // camera
+  createPerspectiveCamera(fov = 50, aspectRatio = 1, near = 0.1, far = 2000) {
+    return new THREE.PerspectiveCamera(fov, aspectRatio, near, far);
+  },
+  // light
+  createDirectionalLight(color = 0xffffff, intensity = 1) {
+    return new THREE.DirectionalLight(color, intensity);
+  },
+  // geometry
+  createPlaneGeometry(width = 1, height = 1, widthSegaments = 1, heightSegments = 1) {
+    const geometry = new THREE.PlaneGeometry(width, height, widthSegaments, heightSegments);
+    return geometry;
+  },
+  createBoxGeometry(width = 1, height = 1, depth = 1) {
+    const geometry = new THREE.BoxGeometry(width, height, depth);
+    return geometry;
+  },
+  // material
+  createMeshStandardMaterial() {
+    const material = new THREE.MeshStandardMaterial();
+    return material;
+  },
+  // mesh
+  createMeshObj(geometry, material) {
+    const meshObj = new THREE.Mesh(geometry, material);
+    return meshObj;
+  },
+  // object
+  createPlane(width = 1, height = 1, widthSegaments = 1, heightSegments = 1) {
+    const planeGeometry = this.createPlaneGeometry(width, height, widthSegaments, heightSegments);
+    const meshStandardMaterial = this.createMeshStandardMaterial();
+    return this.createMeshObj(planeGeometry, meshStandardMaterial);
+  },
+  createCube(width = 1, height = 1, depth = 1) {
+    const boxGeometry = this.createBoxGeometry(width, height, depth);
+    const meshStandardMaterial = this.createMeshStandardMaterial();
+    return this.createMeshObj(boxGeometry, meshStandardMaterial);
+  },
+  // createHelpers
+  createCameraHelper(camera) {
+    const cameraHelper = new THREE.CameraHelper(camera);
+    return cameraHelper;
+  },
+  createDirectionalRightHelper(light) {
+    const lightHelper = new THREE.DirectionalLightHelper(light);
+    return lightHelper;
+  },
+  // methods
+  setPosition(obj, { x = null, y = null, z = null }) {
+    obj.position.set(x || obj.position.x, y || obj.position.y, z || obj.position.z);
+  },
+  setRotation(obj, { x = null, y = null, z = null }) {
+    obj.rotation.set(x || obj.rotation.x, y || obj.rotation.y, z || obj.rotation.z);
+  },
 };
 
 export {
--- src/components/pages/GameMain.vue
+++ src/components/pages/GameMain.vue
@@ -1,14 +1,99 @@
 <template>
-  <div class='game-main'>
-    gameMain
+  <div class="game-main">
+    <canvas ref="canvas" class="canvas"></canvas>
   </div>
 </template>
 
 <script>
 export default {
   name: 'GameMain',
+
+  data() {
+    return {
+      cameraFov: 100,
+      cameraFrustumNearPlane: 0.1,
+      cameraFrustumFarPlane: 1000,
+      directionalLightColor: 0xFFFFFF,
+      directionalLightIntensity: 1,
+      renderer: null,
+    };
+  },
+
+  computed: {
+    canvasWidth() {
+      return this.$refs.canvas.clientWidth;
+    },
+    canvasHeight() {
+      return this.$refs.canvas.clientHeight;
+    },
+    aspectRatio() {
+      return this.canvasWidth / this.canvasHeight;
+    },
+    camera() {
+      return this.$customThree.createPerspectiveCamera(
+        this.cameraFov,
+        this.aspectRatio,
+        this.cameraFrustumNearPlane,
+        this.cameraFrustumFarPlane,
+      );
+    },
+    directionalLight() {
+      return this.$customThree.createDirectionalLight(
+        this.directionalLightColor,
+        this.directionalLightIntensity,
+      );
+    },
+    cube() {
+      return this.$customThree.createCube(100, 100, 100);
+    },
+    plane() {
+      return this.$customThree.createPlane(150, 150, 64, 64);
+    },
+    // helpers
+    cameraHelper() {
+      return this.$customThree.createCameraHelper(this.camera);
+    },
+    directionalLightHelper() {
+      return this.$customThree.createDirectionalRightHelper(this.directionalLight);
+    },
+  },
+
+  mounted() {
+    // createRenderer
+    this.renderer = this.$customThree.createWebGLRenderer({
+      antialias: true,
+      canvas: this.$refs.canvas,
+      alpha: false,
+    });
+    // createScene
+    const scene = this.$customThree.createScene();
+    // setPosition, setRotation
+    this.$customThree.setPosition(this.camera, { x: 0, y: 60, z: 140 });
+    this.$customThree.setRotation(this.camera, { x: Math.PI / -4 });
+    this.$customThree.setPosition(this.directionalLight, { x: 0, y: 150, z: 0 });
+    this.$customThree.setRotation(this.plane, { x: Math.PI / -2 });
+    this.$customThree.setRotation(this.cube, { x: Math.PI / 4, y: Math.PI / 4 });
+    // add scene
+    scene.add(
+      this.directionalLight,
+      this.cube,
+      this.plane,
+      this.directionalLightHelper,
+      this.cameraHelper,
+    );
+    // render
+    this.renderer.render(scene, this.camera);
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+.game-main{
+  width: 100vw;
+  height: 100vh;
+  .canvas{
+    width: 100%;
+    height: 100%;
+  }
+}
 </style>
