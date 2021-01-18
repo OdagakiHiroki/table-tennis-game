1e67d6a0ac188bf547fb9590ea130c53a2c4ecd3
OdagakiHiroki <email@example.com>
2021-01-18 22:30:46 +0900

    mod routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '@/components/pages/Home.vue';
-import About from '@/components/pages/About.vue';
+// import Home from '@/components/pages/Home.vue';
+// import About from '@/components/pages/About.vue';
 import GameTop from '@/components/pages/GameTop.vue';
 import GameMain from '@/components/pages/GameMain.vue';
 import GameResult from '@/components/pages/GameResult.vue';
@@ -9,23 +9,23 @@ import GameResult from '@/components/pages/GameResult.vue';
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About,
-  },
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home,
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   component: About,
+  // },
   {
     path: '/game-top',
     name: 'GameTop',
     component: GameTop,
   },
   {
-    path: '/game-main',
+    path: '/',
     name: 'GameMain',
     component: GameMain,
   },
